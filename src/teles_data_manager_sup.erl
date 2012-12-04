d6ba999a70d8e5c3042db052de079c9c195f9dfc
Armon Dadgar <armon.dadgar@gmail.com>
2012-12-04 13:50:30 -0800

    Split start_agents into 2, so you can start a single agent

--- src/teles_data_manager_sup.erl
+++ src/teles_data_manager_sup.erl
@@ -1,6 +1,6 @@
 -module(teles_data_manager_sup).
 -behaviour(supervisor).
--export([start_link/1, init/1, start_agents/2]).
+-export([start_link/1, init/1, start_agents/2, start_agent/2]).
 
 start_link(NumAgents) ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, [NumAgents]).
@@ -20,15 +20,20 @@ start_agents(Num, Space) ->
 
 start_agents(0, _, Results) -> Results;
 start_agents(Num, Space, Results) ->
+    Res = start_agent(Num, Space),
+    start_agents(Num - 1, Space, [Res | Results]).
+
+
+% Starts a single agent
+start_agent(Num, Space) ->
     AgentSpec = {{agent, Space, Num},
                 {teles_data_agent, start_link, [Num, Space]},
                 temporary, 5000, worker, dynamic},
 
-    % Start the child
-    Res = case supervisor:start_child(?MODULE, AgentSpec) of
+    case supervisor:start_child(?MODULE, AgentSpec) of
         {ok, Pid} -> {new, Pid};
         {error, {already_started, Pid}} -> {exists, Pid};
         {error, _} -> error
-    end,
-    start_agents(Num - 1, Space, [Res | Results]).
+    end.
+
 
